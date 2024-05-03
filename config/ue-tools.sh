9da6ba8346538a0410926b4496f2e9143daf2d98
edierbra <edierbra@unicauca.edu.co>
2024-05-02 22:14:38 -0400

    Configure ran, ue and amf ip addresses

---
+++ config/ue-tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Instalar net-tools
+apt-get update && apt-get install -y net-tools
+# Instalar tcpdump
+apt-get install -y tcpdump
+# Instalar traceroute
+apt-get install -y traceroute
+
